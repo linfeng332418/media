bc2658a6660e04143e2318d87663d4b26f035b59
章林峰 <linfeng.zhang@naxions.com>
2021-12-12 22:48:02 +0800

    增加了类

--- common/service-base/src/main/java/com/linfeng/servicebase/handler/abc.java
+++ common/service-base/src/main/java/com/linfeng/servicebase/handler/abc.java
@@ -3,5 +3,7 @@ package com.linfeng.servicebase.handler;
 public class abc {
     public static void main(String[] args) {
         System.out.println("dnhabdabhd");
+        System.out.println("02");
+        System.out.println("zlf_02");
     }
 }
